d2a230e155874cf66c6fe2c244114e1921020277
Andrew116-del <5101329t@gmail.com>
2020-03-18 00:22:14 +0300

    Homework implimited

--- src/lesson1/homework1/homework1.java
+++ src/lesson1/homework1/homework1.java
@@ -3,6 +3,6 @@ package lesson1.homework1;
 public class homework1 {
     public static void main(String[] args) {
         //ctrl + a
-        System.out.println();
+        System.out.println("ОК");
     }
 }
