2f5a9521341cab0ea2058c0e0552f367a3e0cfe1
Andrew116-del <5101329t@gmail.com>
2020-03-21 18:39:58 +0300

    Homework implimented

--- src/lesson1/homework1/homework1.java
+++ src/lesson1/homework1/homework1.java
@@ -3,6 +3,10 @@ package lesson1.homework1;
 public class homework1 {
     public static void main(String[] args) {
 
+        int i = 12;
+
+        System.out.println("Вывод данных" + i);
+
 
     }
 }
